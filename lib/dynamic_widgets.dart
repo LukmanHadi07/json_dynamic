7ad6e876c1863d3edc319c804a850d74e27cec7d
Lukman Hadi <lukmanhadi2000@gmail.com>
2024-05-06 15:11:21 +0700

    initial

---
+++ lib/dynamic_widgets.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:json_dynamic_widget/json_dynamic_widget.dart';
+
+class DynamicWidgets extends StatefulWidget {
+  const DynamicWidgets({super.key});
+
+  @override
+  State<DynamicWidgets> createState() => _DynamicWidgetsState();
+}
+
+class _DynamicWidgetsState extends State<DynamicWidgets> {
+
+  Map dataWidgets  = {
+    "type" : "Scaffold",
+     "args" : {
+      "body" : {
+      "type" : "center",
+      "child" : {
+        "type" : "circular_progress_indicator"
+      }
+    }
+     }
+  };
+
+  var registry = JsonWidgetRegistry.instance;
+
+  Future<Map> readDataJson() async {
+    final String autoAnimateJson = await rootBundle.loadString('assets/json/auto_animate.json');
+    var json = jsonDecode(autoAnimateJson);
+    return json;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+     readDataJson().then((value) => dataWidgets = value);
+    var widget = JsonWidgetData.fromDynamic(dataWidgets, registry: registry);
+    return widget.build(context: context);
+  }
+}
